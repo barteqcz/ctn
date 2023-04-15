4d4dd54fdf73c0006ba87ef6f1d6beb17872a287
barteqcz <118764028+barteqcz@users.noreply.github.com>
2023-04-15 18:33:07 +0200

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ var g_skinConfig = {
     "peerJSkey": "damhc79361fhto6r",        //API key for PeerJS (remote control), no reason to change this yet
     "url-prefix": "",
     "ets2": {
-        "map": "cities/cities-ets2.js",                //What map you're using, this is default + ProMods + RusMap + Project Balkans + Egypt add-on by default. Pick one of the files in the cities folder.
+        "map": "cities/cities-ets2.js",                //What map you're using, this is base game + ProMods + Promods Middle East + rusMap.
         "stations": "stations/stations-europe.js",     //What stations file to use, currently only stations-europe available.
     },
     "ats": {
